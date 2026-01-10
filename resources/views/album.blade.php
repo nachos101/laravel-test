c715cc7e521fa120af741e008438f72d60b7da54
nachos101 <nachostrau17@gmail.com>
2026-01-10 16:18:38 -0300

    first commit

---
+++ resources/views/album.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Soundy</title>
+</head>
+<body>
+    <header>
+        <h1>Soundy</h1>
+        <nav>
+            
+        </nav>
+    </header>
+    
+    <footer>
+        <p>© 2026 Soundy. Todos los derechos reservados.</p>
+    </footer>
+    
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Soundy</title>
+</head>
+<body>
+    <header>
+        <h1>Soundy</h1>
+        <nav>
+            <a href="/">Inicio</a>
+            <a href="/suma">Suma</a>
+        </nav>
+    </header>
+    <p>Bienvenido a Soundy, aqui podras escuchar la musica que desees.</p>
+    <footer>
+        <p>© 2026 Soundy. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
