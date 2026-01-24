3f5555332c0a22d6bcd939abcef39d4bbee2cc35
nachos101 <nachostrau17@gmail.com>
2026-01-24 12:08:55 -0300

    session added

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Album extends Model
 {
-    //
+    protected $fillable = ['name','artist','genre','release_date','cover_image_url','price'];
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use App\Http\Controllers\AlbumController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pop',[AlbumController::class, 'showAllAlbumsPop']);
-Route::get('/trap',[AlbumController::class, 'showAllAlbumsTrap']);
-Route::get('/rock_nacional',[AlbumController::class, 'showAllAlbumsRockNacional']);
-Route::get('/rock_alternativo',[AlbumController::class, 'showAllAlbumsRockAlternativo']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
